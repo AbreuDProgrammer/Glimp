dea2bca42f57cccaae365de2f9c48810d9ab01c7
Leonardo <leo.abreudp@gmail.com>
2022-12-16 09:10:06 +0000

    fotos

--- application/views/includes/menu.php
+++ application/views/includes/menu.php
@@ -1,8 +1,8 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <div id="sidebar">
-    <div class="item-sidebar"><a href="<?= base_url(); ?>"><img id="logo-sidebar" src="<?= base_url('assets/images/glimp-logo-small.png'); ?>"></a></div>
-    <div class="item-sidebar"><a href="<?= base_url('search'); ?>"><img class="href-sidebar" src="<?= base_url('assets/images/search.png'); ?>"></a></div>
-    <div class="item-sidebar"><a href="<?= base_url('friends'); ?>"><img class="href-sidebar" src="<?= base_url('assets/images/friends.png'); ?>"></a></div>
-    <div class="item-sidebar"><a href="<?= base_url($profile); ?>"><img class="href-sidebar" src="<?= base_url('assets/images/profile.png'); ?>"></a></div>
-    <div class="item-sidebar"><a href="<?= base_url('logout'); ?>"><img class="href-sidebar" src="<?= base_url('assets/images/logout.png'); ?>"></a></div>
+    <div class="item-sidebar"><a href="<?= base_url(); ?>"><img id="logo-sidebar" src="<?= base_url('assets/images/logo-danilo.png'); ?>"></a></div>
+    <div class="item-sidebar"><a href="<?= base_url('search'); ?>"><img class="href-sidebar" src="<?= base_url('assets/images/search-danilo-t.png'); ?>"></a></div>
+    <div class="item-sidebar"><a href="<?= base_url('friends'); ?>"><img class="href-sidebar" src="<?= base_url('assets/images/friends-danilo-t.png'); ?>"></a></div>
+    <div class="item-sidebar"><a href="<?= base_url($profile); ?>"><img class="href-sidebar" src="<?= base_url('assets/images/profile-danilo-t.png'); ?>"></a></div>
+    <div class="item-sidebar"><a href="<?= base_url('logout'); ?>"><img class="href-sidebar" src="<?= base_url('assets/images/logout-danilo-t.png'); ?>"></a></div>
 </div>
