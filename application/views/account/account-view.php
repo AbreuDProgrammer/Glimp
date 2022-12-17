38f45fb52c4cc01381da7ca15baac96561f90887
Leonardo <leo.abreudp@gmail.com>
2022-12-17 21:56:37 +0000

    mínimo de css no account

--- application/views/account/account-view.php
+++ application/views/account/account-view.php
@@ -1,12 +1,29 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-<div id="account-page">
-    <form method="post">
-        <input type="hidden" value="<?= $user_id; ?>" name="user_id">
+<h1 id="page_title">Account Settings</h1>
+<form method="post" id="user_form">
+    <input type="hidden" value="<?= $user_id; ?>" name="user_id">
+    <div class="form_item">
+        <h2>Username</h2>
         <input value="<?= $username; ?>" type="text" name="username" placeholder="Username">
+    </div>
+    <div class="form_item">
+        <h2>Email</h2>
         <input value="<?= $email; ?>" type="email" name="email" placeholder="Email">
+    </div>
+    <div class="form_item">
+        <h2>Phone number</h2>
         <input value="" type="tel" name="phone" placeholder="Phone">
+    </div>
+    <div class="form_item">
+        <h2>Name</h2>
         <input value="" type="text" name="name" placeholder="Name">
+    </div>
+    <div class="form_item">
+        <h2>Birthday</h2>
         <input value="" type="date" name="birthday" placeholder="Birthday">
-        <input type="submit">
-    </form>
+    </div>
+    <input type="submit" value="Submit">
+</form>
+<div id="form_infos">
+    <?= $error['form_info']; ?>
 </div>
